ed802a31b25f8cec74a736d29421eac69f0ceb09
David Barker <dave@ncomtech.com>
2023-03-23 09:16:11 -0600

    created directory structure for pages and images in a new dist deployment directory

---
+++ dist/themeset.js
@@ -0,0 +1,22 @@
+function themeSet(themeName) {
+    let theme = document.getElementsByTagName('link')[0];
+    theme.setAttribute('href', themeName);
+}
+
+function setThemeOnClick() {
+    let list = document.getElementById('themelist');
+    let listItems = list.getElementsByTagName('li');
+    [...listItems].forEach(element => {
+        if ("Float" == element.innerHTML) {
+            element.setAttribute('onclick', "themeSet('beerskis.css')");
+        } else if ("Grid" == element.innerHTML) {
+            element.setAttribute('onclick', "themeSet('beerskis-grid.css')");
+        } else {
+            element.setAttribute('onclick', "themeSet('beerskis-flex.css')");
+        }
+    });
+}
+
+window.onload = () => {
+    setThemeOnClick();
+}
