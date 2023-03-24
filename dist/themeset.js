acaa632a0103a0112e22a67f667d91f307e1badd
Tim Mc <mccaff3456@gmail.com>
2023-03-24 11:00:45 -0400

    Clean up .css files and update Beerskis.css

--- dist/themeset.js
+++ dist/themeset.js
@@ -2,7 +2,7 @@ function themeSet(themeName) {
     let theme = document.getElementsByTagName('link')[0];
     theme.setAttribute('href', themeName);
 }
-alert("This is an alert")
+// alert("This is an alert")
 function setThemeOnClick() {
     let list = document.getElementById('themelist');
     let listItems = list.getElementsByTagName('li');
@@ -21,9 +21,20 @@ window.onload = () => {
     setThemeOnClick();
 }
    
-function openNav() {
+/* function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
       }
 function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
       }
+      */
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("content").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("content").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+    }
