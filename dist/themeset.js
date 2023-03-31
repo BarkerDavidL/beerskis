e4d01f3aa98278cb50f8129cfd2daf18bbb35d30
David Barker <dave@ncomtech.com>
2023-03-31 11:13:36 -0400

    changed selection from the deleted ID to use the class name for sideNav

--- dist/themeset.js
+++ dist/themeset.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsByClassName("sidenav")[0].style.width = "250px";
     //document.getElementById("content").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementsByClassName("sidenav")[0].style.width = "0";
     //document.getElementById("content").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
