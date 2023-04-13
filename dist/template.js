278b21e4b1dee5a2edda2bb26641ba4505556eb3
Tim Mc <mccaff3456@gmail.com>
2023-04-13 10:11:21 -0400

    Updated Reuse Header

--- dist/template.js
+++ dist/template.js
@@ -2,26 +2,23 @@ const template = document.createElement('template');
 
 template.innerHTML = `
 <header>        
-<span onclick="openNav()">&#9776;</span>
-<h1>Dave and Tim Adventures</h1>
-<a href="./pages/temp.html">About</a>
+    <span onclick="openTopNav()">&#9776;</span>
+    <h1>Da Beerskis Time Portal</h1>
+    <a href="./pages/temp.html">About</a>
 
-<div class="sidenav">
-<span class="closebtn" onclick="closeNav()">&times;</span>
-<a href="./pages/lodging.html">Lodging</a>
-<a href="./pages/beers.html">Brewpubs</a>
-<a href="./pages/packages.html">All-Inclusives</a>
-<a href="./pages/flights.html">Flights</a>
-</div>
-<div>
-    <span class="breadcrumbs">
-    <a href="../index.html">Home</a> ->
-    </span>
-</div>
-</header>
+  <div class="topNav">
+    <span class="closebtn" onclick="closeTopNav()">&times;</span>
+    <a href="./pages/lodging.html">Lodging</a>
+    <a href="./pages/beers.html">Brewpubs</a>
+    <a href="./pages/packages.html">All-Inclusives</a>
+    <a href="./pages/flights.html">Flights</a>
+  </div>
+ </header>
+
 <footer>
 <p>Beerski Travel Inc. &#169;2023</p>
 </footer>
 `;
 
 document.body.appendChild(template.content);
+
--- dist/themeset.js
+++ dist/themeset.js
@@ -9,3 +9,14 @@ function closeNav() {
     //document.getElementById("content").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
+function openTopNav() {
+    document.getElementsByClassName("topNav")[0].style.width = "250px";
+    //document.getElementById("content").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeTopNav() {
+    document.getElementsByClassName("topNav")[0].style.width = "0";
+    //document.getElementById("content").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
