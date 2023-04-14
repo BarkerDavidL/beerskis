ad6bcbc806590bfb97da658e5c503da750105f03
Tim Mc <mccaff3456@gmail.com>
2023-04-14 10:24:31 -0400

    Update template.js and Child Pages

--- dist/template.js
+++ dist/template.js
@@ -1,17 +1,22 @@
 const template = document.createElement('template');
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log(page);
+var pathName = "./pages/"
 
 template.innerHTML = `
-<header>        
+<header>     
     <span onclick="openTopNav()">&#9776;</span>
     <h1>Da Beerskis Time Portal</h1>
     <a href="./pages/temp.html">About</a>
 
   <div class="topNav">
     <span class="closebtn" onclick="closeTopNav()">&times;</span>
-    <a href="./pages/lodging.html">Lodging</a>
-    <a href="./pages/beers.html">Brewpubs</a>
-    <a href="./pages/packages.html">All-Inclusives</a>
-    <a href="./pages/flights.html">Flights</a>
+    
+    <a href= "${pathName}lodging.html">Lodging</a>
+    <a href="/dist/pages/beers.html">Brewpubs</a>
+    <a href="/dist/pages/packages.html">All-Inclusives</a>
+    <a href="/dist/pages/flights.html">Flights</a>
   </div>
  </header>
 
