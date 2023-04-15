8109aaa30ca4354c82c8ac5d3f92f5b9fb6d9e46
Tim Mc <mccaff3456@gmail.com>
2023-04-14 22:20:54 -0400

    Push changes to GitHub

--- dist/template.js
+++ dist/template.js
@@ -1,22 +1,37 @@
-const template = document.createElement('template');
-var path = window.location.pathname;
+let template = document.createElement('template');
+var path = location.pathname;
 var page = path.split("/").pop();
-console.log(page);
-var pathName = "./pages/"
+console.log("Page = ", page);
+console.log("Path = ", path);
+if (page="index.html") {
+  var pathName = ""; }
+else {var pathName = "";}
+console.log("PathName = ", pathName);
+
 
 template.innerHTML = `
+<head>
+<title>Da Beerskis Time Portal</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta name="author" content="Beerskis Travel Inc.">
+<meta name="description" content="Travel with skiing, snowboarding beer drinking and friends">
+<meta name="keywords" content="skiing,travel,brews,beer,ski,snowboard,snowboarding">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./beerskis.css">
+
+</head>
 <header>     
     <span onclick="openTopNav()">&#9776;</span>
     <h1>Da Beerskis Time Portal</h1>
-    <a href="./pages/temp.html">About</a>
-
-  <div class="topNav">
+    <a href="/dist/pages/temp.html">About</a>
+   <div class="topNav">
     <span class="closebtn" onclick="closeTopNav()">&times;</span>
-    
-    <a href= "${pathName}lodging.html">Lodging</a>
+    <a href= "/dist/index.html">Home</a>
+    <a href= "/dist/pages/lodging.html">Lodging</a>
     <a href="/dist/pages/beers.html">Brewpubs</a>
     <a href="/dist/pages/packages.html">All-Inclusives</a>
-    <a href="/dist/pages/flights.html">Flights</a>
+    <a href="/dist/pages/lightBox.html">Beerski Trips</a>
   </div>
  </header>
 `;
@@ -38,3 +53,25 @@ function loadFooter() {
 }
 
 loadFooter();
+function openNav() {
+  document.getElementsByClassName("sidenav")[0].style.width = "250px";
+  //document.getElementById("content").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementsByClassName("sidenav")[0].style.width = "0";
+  //document.getElementById("content").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+function openTopNav() {
+  document.getElementsByClassName("topNav")[0].style.width = "250px";
+  //document.getElementById("content").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeTopNav() {
+  document.getElementsByClassName("topNav")[0].style.width = "0";
+  //document.getElementById("content").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
