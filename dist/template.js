6a7e89b5568a8395ca80c636de8d996d071182ed
David Barker <dave@ncomtech.com>
2023-05-19 11:29:28 -0400

    fixed navigation links and link to css file in all pages

--- dist/template.js
+++ dist/template.js
@@ -21,7 +21,7 @@ template.innerHTML = `
 <header>     
     <span onclick="openTopNav()">&#9776;</span>
     <h1>Da Beerskis Time Portal</h1>
-    <a href="/dist/pages/temp.html">About</a>
+    <a href="./temp.html">About</a>
   <div class="topNav">
     <span class="closebtn" onclick="closeTopNav()">&times;</span>
     <a href= ".${rootPath}/index.html">Home</a>
