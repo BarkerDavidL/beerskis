26c003af0475d6410a5ee4bd479ba33f3ce417a5
David Barker <dave@ncomtech.com>
2023-06-22 18:28:09 -0400

    first draft of seeing trips by person - only for WV trips for now

--- dist/eventsbyperson.js
+++ dist/eventsbyperson.js
@@ -1,6 +1,6 @@
 /**
  * 
- * getAttendees - reads a JSON file and looks for a column matching the 'year'
+ * listPerson - reads a JSON file and looks for a column matching the 'year'
  *                then returns a table entry for each column containing a '1'
  * 
  *                This function is called from each trip page to display the
@@ -12,7 +12,10 @@
  * @param {string} fileurl - the URL of the JSON file containing attendee data
  * @param {string} year - the column heading of the attendee data to display
  */
-function getAttendees(fileurl, year) {
+
+let attendee_list = {};
+
+function listNames(fileurl) {
     fetch(fileurl)
     .then(function(response) {
         if (!response.ok) {
@@ -21,27 +24,58 @@ function getAttendees(fileurl, year) {
         return response.json();
     })
     .then(function(attendees) {
+        attendee_list = attendees;
+        let name_list = document.querySelector("#person");
+        let out = "";
+        console.log("Starting name list");
+        for (let attendee of attendees) {
+            if (!["", "Name"].includes(attendee.Name)) {
+                console.log(attendee.Name);
+                out += `
+                    <option value="${attendee.Name}">${attendee.Name}</option>
+                `;
+            }
+        }
+
+        name_list.innerHTML = out;
+    });
+}
+
+function listPerson(person) {
         let attendee_table = document.querySelector("#attendee_output");
         let out = "";
-        let attendee_count = 0;
-        console.log("Year: " + year);
-        for (let attendee of attendees) {
-            if (attendee[year] != "") {
+        console.log("Starting attendee list");
+        for (let attendee of attendee_list) {
+            if (person == attendee.Name) {
+                let trip_info = "";
+                let num_events = 0;
+                for (col in attendee) {
+                    if (!["", "Name"].includes(col)) {
+                        if ("" != attendee[col]) {
+                            trip_info += `
+                                <li>${col}</li>
+                            `;
+                            num_events += 1;
+                        }
+                    }
+                }
+                console.log(attendee.Name + ": " + num_events);
                 out += `
-                <tr>
-                    <td>${attendee.Name}</td>
-                </tr>
+                <div>
+                    <h3>${attendee.Name}</h3>
+                    <ul>
+                    ${trip_info}
+                    </ul>
+                    <p>${num_events} trips</p>
+                </div>
                 `;
-                attendee_count += 1;
             }
         }
 
         attendee_table.innerHTML = out;
-        document.getElementById('rowCount').innerHTML = attendee_count + " Attendees";
-    });
 }
 
 let fileurl = document.currentScript.getAttribute('fileurl');
-let year = document.currentScript.getAttribute('year');
+let person = document.currentScript.getAttribute('person');
 
-getAttendees(fileurl, year);
+listNames(fileurl);
